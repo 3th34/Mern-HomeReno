74ff67541643934017919aa5a2a0f53f4e18abe0
reggie <attentionpaysleast@proton.me>
2023-12-21 17:21:30 -0500

    Created Pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AboutMe from "./assets/AboutMe";
+import ContactMe from "./assets/ContactMe";
+import IndustryKnowHow from "./assets/IndustryKnowHow";
+import PastProjects from "./assets/PastProjects";
+import Thoughts from "./assets/Thoughts";
+import Signin from "./assets/Signin";
+import Signup from "./assets/Signup";
 
 export default function App() {
-  return <h1 className='text-red-500'>Quayson</h1>
+  return <BrowserRouter>
+  <Routes>
+    <Routes path="/Home" element={<Home />} />
+    <Routes path="/AboutMe" element={<AboutMe />} />
+    <Routes path="/ContactMe" element={<ContactMe />} />
+    <Routes path="/IndustryKnowHow" element={<IndustryKnowHow/>} />
+    <Routes path="/PastProjects" element={<PastProjects/>} />
+    <Routes path="/Thoughts" element={<Thoughts/>} />
+    <Routes path="/Signin" element={<Signin/>} />
+    <Routes path="/Signup" element={<Signup />} />
+  </Routes>
+  </BrowserRouter>
 }
