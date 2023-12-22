0be320222c9b58b869197bfffb8f627daf001350
reggie <attentionpaysleast@proton.me>
2023-12-22 15:51:17 -0500

    Connect to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+
+mongoose.connect(process.env.Test).then(() => {
+    console.log('Connected to MongoDB!');
+    }).catch((err) => {
+        console.log(err);
+    });
 const app = express();
 
 app.listen(3000, () => {
